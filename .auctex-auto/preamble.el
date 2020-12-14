54b814c8af6f87ecf2cccbe4fe86e0601e917aeb
latenighticecream <latenighticecream@github.io>
2020-12-14 18:13:46 +0100

    update

--- .auctex-auto/preamble.el
+++ .auctex-auto/preamble.el
@@ -2,7 +2,18 @@
  "preamble"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("babel" "ngerman") ("inputenc" "utf8") ("inconsolata" "scaled=1") ("fontenc" "T1")))
+                     '(("babel" "ngerman") ("inputenc" "utf8") ("inconsolata" "scaled=1")))
+   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "babel"
     "inputenc"
@@ -14,7 +25,7 @@
     "listings"
     "graphicx"
     "inconsolata"
-    "fontenc"
+    "fontspec"
     "subfig"
     "titling"
     "hyperref")
@@ -23,6 +34,23 @@
     '("subtitle" 1)
     "solution"
     "hinweis"
-    "oldemph"))
+    "oldemph")
+   (LaTeX-add-enumitem-newlists
+    '("checklist" "itemize"))
+   (LaTeX-add-xcolor-definecolors
+    "commentColor"
+    "mygray"
+    "stringColor"
+    "keywordColor"
+    "backgroundColor"
+    "borderColor"
+    "inlineTextColor"
+    "linkcolor"
+    "yelluw0"
+    "yelluw"
+    "yelluw2"
+    "yelluw3"
+    "limeu"
+    "mordantred19"))
  :latex)
 
